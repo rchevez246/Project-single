f25812305b3899fc757042f028706adbea5cdf84
Chevez <ricky.chevez24601@gmail.com>
2022-03-25 15:22:31 -0600

    init files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from utils.db import db
+from routes.ruta101b import colinf
+from Databases.db import db
 
 app = Flask(__name__)
 
@@ -9,3 +10,4 @@ app.config.from_object("config.BaseConfig")
 SQLAlchemy(app)
 
 # register blueprints
+app.register_blueprint(colinf)
